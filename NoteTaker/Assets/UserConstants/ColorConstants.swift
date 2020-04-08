d2283a99b9e21b6fec47872d216cf3a131ce2a89
Ian McDonald <ianjoemcdonald@gmail.com>
2020-04-07 21:30:13 -0300

    Adding Color Constants Enum
    Because:
    - These color constants will be used throughout the app and it is better
      for them not to be "stringly" typed.
    - The enum allows them to be used without writting a string.
    
    This was part of day 7 of 100DaysOfCode. Using this allows the colors to be
    used anywhere in the app rather than declaring them locally.

---
+++ NoteTaker/Assets/UserConstants/ColorConstants.swift
@@ -0,0 +1,29 @@
+//
+//  ColorConstants.swift
+//  NoteTaker
+//
+//  Created by Ian McDonald on 07/04/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+
+import Foundation
+
+enum ColorConstants {
+    static let teal = "Teal"
+    static let blue = "Blue"
+    static let indigo = "Indigo"
+    static let purple = "Purple"
+    static let magenta = "Magenta"
+    static let red = "Red"
+    static let orange = "Orange"
+    static let brown = "Brown"
+    static let tan = "Tan"
+    static let yellow = "Yellow"
+    static let lime = "Lime"
+    static let green = "Green"
+    
+    static let array = [ColorConstants.teal, ColorConstants.blue, ColorConstants.indigo,
+                        ColorConstants.purple, ColorConstants.magenta, ColorConstants.red,
+                        ColorConstants.orange, ColorConstants.brown, ColorConstants.tan,
+                        ColorConstants.yellow, ColorConstants.lime, ColorConstants.green]
+}
